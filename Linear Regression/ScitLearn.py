c63451b57d00bc769a20d5ddaa99a8919e201084
Pramodh <pramodh380@gmail.com>
2024-02-14 16:29:55 +0530

    Linear Regression

---
+++ Linear Regression/ScitLearn.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import mean_absolute_error
+
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                             ])
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
